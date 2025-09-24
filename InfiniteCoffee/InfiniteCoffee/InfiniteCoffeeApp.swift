eaaded7e1deff4e8d28bc0d88102b6abdb71fd0b
Alysson Menezes Dodo <almenezes912@gmail.com>
2025-09-24 19:26:08 -0300

    start project + initial colors

---
+++ InfiniteCoffee/InfiniteCoffee/InfiniteCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCoffeeApp.swift
+//  InfiniteCoffee
+//
+//  Created by ALYSSON MENEZES on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
